752afdba52f90c1595b8da7453eb5e35d25f9b1c
zeapiaji <89508245+zeapiaji@users.noreply.github.com>
2021-10-30 15:19:51 +0700

    Add files via upload

---
+++ navbar.php
@@ -0,0 +1,37 @@
+
+<nav class="navbar navbar-light bg-light mb-3 shadow-sm">
+  <div class="container-fluid">
+    <a class="navbar-brand" href="#">TabunganQ</a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasNavbar" aria-controls="offcanvasNavbar">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <!-- Navbar offcanvas -->
+    <div class="offcanvas offcanvas-end" tabindex="-1" id="offcanvasNavbar" aria-labelledby="offcanvasNavbarLabel" style="background-image: url(src/img/wallpaper.png);">
+      <div class="offcanvas-header">
+        <h5 class="offcanvas-title text-light" id="offcanvasNavbarLabel" style="">TabunganQ</h5>
+        <button type="button" class="btn-close text-reset" data-bs-dismiss="offcanvas" aria-label="Close"></button>
+      </div>
+      <div class="offcanvas-body">
+        <ul class="navbar-nav justify-content-end flex-grow-1 pe-3">
+          <li class="nav-item">
+            <a class="bx bx-home mt-3 fs-5 text-light" aria-current="page" href="#" style="text-decoration: none;">Home</a>
+          </li>
+           <li class="nav-item">
+            <a class="bx bx-bar-chart-alt 2 mt-2 fs-5 text-light" href="#" style="text-decoration: none;">Riwayat</a>
+          </li>
+          <li class="nav-item" style="margin-bottom: 350px;">
+            <a class="bx bx-user mt-2 fs-5 text-light" href="#" style="text-decoration: none;">Akun</a>
+          </li>
+          <div class="row">
+            <li class="col-9">
+              asd
+            </li>
+            <li class="nav-item col-3">
+              <a class="fas fa-sign-out-alt mt-3 fs-5 text-danger absolute" aria-current="page" href="#" style="text-decoration: none;">Logout</a>
+            </li>
+          </div>
+        </ul>
+      </div>
+    </div>
+  </div>
+</nav>
